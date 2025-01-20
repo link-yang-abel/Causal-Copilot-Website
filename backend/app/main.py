d06832fcf11a6d50b9af0d41c55225d24afbadd6
link-yang-abel <linky@abel.ai>
2025-01-20 17:19:53 +0800

    backend v0: api.v1.users and api.v1.files.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import users
+from app.api.v1.endpoints import files
 
 app = FastAPI()
 
 app.include_router(users.router, prefix="/api/v1", tags=["users"])
+app.include_router(files.router, prefix="/api/v1", tags=["files"])
 
 if __name__ == "__main__":
     import uvicorn
