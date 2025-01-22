e0b2030aade6fa22eef8b8883b533a8fff549a5b
link-yang-abel <linky@abel.ai>
2025-01-22 18:05:32 +0800

    backend: add cors.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.v1.endpoints import users
 from app.api.v1.endpoints import files
 
 app = FastAPI()
+
+origins = [
+    '*',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix="/api/v1", tags=["users"])
 app.include_router(files.router, prefix="/api/v1", tags=["files"])
